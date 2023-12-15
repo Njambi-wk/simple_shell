c4d8edb269812cbd6f532db2aa95afff782152e4
njambi-wk <njambi.karungari01@gmail.com>
2023-12-15 11:32:19 -0800

    env

--- env.c
+++ env.c
@@ -4,30 +4,43 @@
 
 #define MAX_COMMAND_LENGTH 100
 
+void print_environment() 
+{
+    extern char **environ;
+    int i;
+
+    for (i = 0; environ[i] != NULL; i++)
+    {
+        printf("%s\n", environ[i]);
+    }
+}
+
 int main()
 {
-	char command[MAX_COMMAND_LENGTH];
+    char command[MAX_COMMAND_LENGTH];
 
-	while (1)
+    while (1)
+    {        
+        printf("$$ ");
+        fgets(command, sizeof(command), stdin);
+
+        command[strcspn(command, "\n")] = '\0';
+        
+        if (strcmp(command, "exit") == 0)
 	{
-		printf("Simple_Shell: ");
-
-		fgets(command, sizeof(command), stdin);
-		command[strcspn(command, "\n")] = '\0';
-
-		if (strcmp(command, "Exit") == 0)
-		{
-			printf("Exit Shell:\n");
-			break;
-		}
-		else if (strcmp(command, "Env") == 0)
-		{
-			system("Env");
-		}
-		else
-		{
-			system(command);
-		}
-	}
-	return (0);
+            printf("Exit shell.\n");
+            break;
+        }
+	else if (strcmp(command, "env") == 0)
+	{
+          
+            print_environment();
+        }
+	else
+	{
+           
+            printf("Command not recognized: %s\n", command);
+        }
+    }
+    return 0;
 }
